f87eb017775eb59bc32ec4a557fab30f1d8236bd
Raghad1M <147823679+Raghad1M@users.noreply.github.com>
2023-12-22 19:40:42 +0300

    main page + app's icon

--- Project/sdd/test/widget_test.dart
+++ Project/sdd/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sdd/main.dart';
+import 'package:Journey/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
