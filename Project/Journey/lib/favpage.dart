536fb0360f0c0b19d62d5afbf49a08695d3f74ae
Raghad1M <147823679+Raghad1M@users.noreply.github.com>
2023-12-24 05:55:58 +0300

    tiny edit

--- Project/Journey/lib/favpage.dart
+++ Project/Journey/lib/favpage.dart
@@ -1,16 +1,53 @@
 import 'package:flutter/material.dart';
-
 class FavoritePage extends StatefulWidget {
   @override
   _FavoritePageState createState() => _FavoritePageState();
 }
 
-class _FavoritePageState extends State<FavoritePage> {
+class _FavoritePageState extends State<FavoritePage> with SingleTickerProviderStateMixin {
+  TabController? _tabController;
   List<String> favorites = [];
+  List<String> filteredFavorites = [];
+  String searchText = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+    _tabController!.addListener(_handleTabChange);
+    filteredFavorites = favorites;
+  }
+
+  @override
+  void dispose() {
+    _tabController!.dispose();
+    super.dispose();
+  }
+
+  void _handleTabChange() {
+    setState(() {
+      // Logic to handle tab changes and filter favorites accordingly
+      switch (_tabController!.index) {
+        case 0:
+          filteredFavorites = favorites; // All
+          break;
+        case 1:
+          filteredFavorites = favorites.where((favorite) => favorite.contains('video')).toList(); // Videos
+          break;
+        case 2:
+          filteredFavorites = favorites.where((favorite) => favorite.contains('article')).toList(); // Articles
+          break;
+        case 3:
+          filteredFavorites = favorites.where((favorite) => favorite.contains('book')).toList(); // Books
+          break;
+      }
+    });
+  }
 
   void addToFavorites(String item) {
     setState(() {
       favorites.add(item);
+      _handleTabChange(); // Update the filtered list when a new item is added
     });
   }
 
@@ -19,25 +56,50 @@ class _FavoritePageState extends State<FavoritePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Favorites'),
-         automaticallyImplyLeading: false, 
         backgroundColor: Color.fromARGB(255, 150, 122, 161),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(text: 'All'),
+            Tab(text: 'Videos'),
+            Tab(text: 'Articles'),
+            Tab(text: 'Books'),
+          ],
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.notifications),
+            onPressed: () {
+              // Add your notification logic here
+            },
+          ),
+        ],
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Favorites:',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Search',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  searchText = value;
+                  _handleTabChange(); // Update the filtered list based on search text
+                });
+              },
             ),
           ),
           Expanded(
             child: ListView.builder(
-              itemCount: favorites.length,
+              itemCount: filteredFavorites.length,
               itemBuilder: (context, index) {
                 return ListTile(
-                  title: Text(favorites[index]),
+                  title: Text(filteredFavorites[index]),
                 );
               },
             ),
--- Project/Journey/lib/user_profile.dart
+++ Project/Journey/lib/user_profile.dart
@@ -7,29 +7,50 @@ class ProfilePage extends StatefulWidget {
   @override
   State<ProfilePage> createState() => _ProfilePageState();
 }
-
 class _ProfilePageState extends State<ProfilePage> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _phoneController = TextEditingController();
-  
-void _handleLogout() async {
-  await FirebaseAuth.instance.signOut();
-  Navigator.pushReplacementNamed(context, '/firstPage');
-}
+  final TextEditingController _oldPasswordController = TextEditingController();
+  final TextEditingController _newPasswordController = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  bool _isEditing = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUserData();
+  }
+
+  Future<void> _fetchUserData() async {
+    try {
+      User? user = _auth.currentUser;
+      if (user != null) {
+        String? name = user.displayName;
+        String? email = user.email;
+
+        setState(() {
+          _nameController.text = name ?? '';
+          _emailController.text = email ?? '';
+        });
+      }
+    } catch (e) {
+      print('Error fetching user data: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Account'),
-        automaticallyImplyLeading: false, 
+        automaticallyImplyLeading: false,
         backgroundColor: Color.fromARGB(255, 150, 122, 161),
-         actions: [
-          IconButton(
-            icon: Icon(Icons.logout), // Icon for logout
-            onPressed: _handleLogout, // Calls the logout function
-          ),
+        actions: [
+          if (!_isEditing)
+            IconButton(
+              icon: Icon(Icons.logout),
+              onPressed: _handleLogout,
+            ),
         ],
       ),
       body: Padding(
@@ -40,48 +61,121 @@ void _handleLogout() async {
             const SizedBox(height: 10.0),
             const CircleAvatar(
               radius: 50.0,
-              backgroundImage: AssetImage('images/Ppic.png',),
+              backgroundImage: AssetImage('images/Ppic.png'),
             ),
             const SizedBox(height: 20.0),
-            TextField(
+            _buildEditableField(
               controller: _nameController,
-              decoration: const InputDecoration(
-                labelText: 'Name',
-              ),
+              label: 'Name',
             ),
             const SizedBox(height: 20.0),
-            TextField(
+            _buildEditableField(
               controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-              ),
+              label: 'Email',
             ),
             const SizedBox(height: 20.0),
-            TextField(
-              controller: _phoneController,
-              decoration: const InputDecoration(
-                labelText: 'Phone number',
-              ),
-            ),
-            const SizedBox(height: 20.0),
-            Center(
-              child: Container(
-                width: double.infinity, // Make the button take the full width
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Save changes logic
-                    // Navigator.of(context).pop();
-                  },
+            if (_isEditing) ...[
+              _buildPasswordFields(),
+              const SizedBox(height: 20.0),
+              _buildSaveButton(),
+            ],
+            if (!_isEditing)
+              ElevatedButton(
+                              
                   style: ElevatedButton.styleFrom(
-                    primary: const Color.fromARGB(255, 150, 122, 161),
+                    primary: Color.fromARGB(255, 150, 122, 161), // Set button color
+                    padding: EdgeInsets.symmetric(horizontal:100, vertical: 15), // Adjust button padding
                   ),
-                  child: const Text('Save changes'),
-                ),
+                onPressed: () {
+                  setState(() {
+                    _isEditing = true;
+                  });
+                },
+                child: const Text('Edit information'),
               ),
-            ),
           ],
         ),
       ),
     );
+  }
+
+  Widget _buildEditableField({
+    required TextEditingController controller,
+    required String label,
+    bool obscureText = false,
+  }) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        
+        labelText: label,
+        enabled: _isEditing,
+      ),
+    );
+  }
+
+  Widget _buildPasswordFields() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TextField(
+          controller: _oldPasswordController,
+          obscureText: true,
+          decoration: InputDecoration(
+            labelText: 'Old Password',
+          ),
+        ),
+        const SizedBox(height: 20.0),
+        TextField(
+          controller: _newPasswordController,
+          obscureText: true,
+          decoration: InputDecoration(
+            labelText: 'New Password',
+          ),
+        ),
+      ],
+    );
+  }
+Widget _buildSaveButton() {
+  return ElevatedButton(
+    onPressed: () async {
+      User? user = _auth.currentUser;
+      if (user != null) {
+        try {
+          await user.updateDisplayName(_nameController.text);
+          await user.updateEmail(_emailController.text);
+          // Update password if provided
+          if (_newPasswordController.text.isNotEmpty) {
+            await user.updatePassword(_newPasswordController.text);
+          }
+          setState(() {
+            _isEditing = false;
+          });
+        } catch (e) {
+          print('Error updating user: $e');
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Error updating user: $e')),
+          );
+        }
+      }
+    },
+    style: ElevatedButton.styleFrom(
+      primary: Color.fromARGB(255, 150, 122, 161), // Set button color
+      padding: EdgeInsets.symmetric(horizontal: 100, vertical: 15), // Adjust button padding
+    ),
+    child: Text('Save Changes'),
+  );
+}
+
+  void _handleLogout() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      Navigator.pushReplacementNamed(context, '/firstPage');
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error signing out: $e')),
+      );
+    }
   }
 }
